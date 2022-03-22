b642a015ac68fd12521c271c7e72e22b68e27a49
Manu07 <e.duplat@free.fr>
2022-03-22 18:44:17 +0100

    Update teamviewer.bat

--- teamviewer.bat
+++ teamviewer.bat
@@ -16,7 +16,7 @@ ECHO Veuillez patienter .
 : Mise a jour
 ECHO Verrification des mises a jour et mise a jpur de l'assistance le cas echeant en cours ..
 start  /wait "%~dp0\git\mingw32\bin\git.exe" pull
-
+pause
 :TeamViewer
 ECHO L'assistance ne va pas tarder a demarrer, nous verifions et nous fermons les autres instances de TeamViewer ...
 START /B /WAIT %SystemRoot%\System32\taskkill.exe /F /im teamviewer.exe > NUL 2>&1
